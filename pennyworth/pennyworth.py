7f3c04c9ecaf5f4bb60614abe98fa3a667612af9
TehPsychedelic <21700114+TehPsychedelic@users.noreply.github.com>
2020-10-03 14:58:18 -0500

    Organized code

--- pennyworth/pennyworth.py
+++ pennyworth/pennyworth.py
@@ -1,28 +1,16 @@
-import os
-
-import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from events.ready import *
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 PREFIX = os.getenv('COMMANDS_PREFIX')
 
 bot = commands.Bot(command_prefix=PREFIX)
 
 @bot.event
 async def on_ready():
-    guild = discord.utils.get(bot.guilds, name=GUILD)
-    print(
-        f'{bot.user.name} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-    print('\tMembers in the guild:')
-    for member in guild.members:
-        print(
-            f'\t\t'
-            f'{member}\n'
-        )
+    ready_debug_msg(bot)
 
 bot.run(TOKEN)
