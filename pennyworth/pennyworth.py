310e3ec00daa8e38fc64fa1099c9e6fb8558dd13
TehPsychedelic <21700114+TehPsychedelic@users.noreply.github.com>
2020-10-03 15:55:51 -0500

    Added reply command

--- pennyworth/pennyworth.py
+++ pennyworth/pennyworth.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 
 from events.ready import *
 from events.member_join import *
+from commands.reply import *
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -20,5 +21,9 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     await new_member_welcome_dm(member, GUILD)
+    
+@bot.command(name='reply')
+async def on_reply_cmd(ctx, arg):
+    await reply_command(ctx, arg)
 
 bot.run(TOKEN)
